cfa64c66e2414f474dc6ae802d49ff2d0caac187
Federico Martinez <federicoemartinez@gmail.com>
2019-12-14 20:32:43 -0300

    Command to compact the queue of changes added

---
+++ deferredCounters/management/commands/compact.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from deferredCounters.models import CounterChanges
+
+
+class Command(BaseCommand):
+    help = 'Apply pending changes to counters'
+
+
+    def handle(self, *args, **options):
+        try:
+            with transaction.atomic():
+                CounterChanges.objects.compact()
+        except Exception as e:
+            raise CommandError('There was an error processing the changes: %s' % e)
+
+        self.stdout.write(self.style.SUCCESS('Successfully processed all the changes'))
+
